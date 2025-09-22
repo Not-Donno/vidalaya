9527398fec08e003279e0ec938f7e40b3151f71c
AbhinavSingh <abhinavgauta123456@gmail.com>
2025-09-22 16:54:09 +0545

    login

---
+++ app/(marketing)/footor.tsx
@@ -0,0 +1,5 @@
+export const Footor = () => {
+  return (
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-20 p-2">Footor</footer>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const font = Nunito({
@@ -19,10 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={`${font.className} antialiased`}>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
